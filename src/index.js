b354fd0541c4035d85a13f30ddcb5c85ff294bd0
Dylan Clements <dclements@protonmail.com>
2020-10-15 21:30:57 -0400

    preliminary iFrame Player

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 document.write("test");
+
+const player = document.getElementById("loopPlayer")
+
+document.getElementById("loopPlayer").innerHTML =  <iframe width="560" height="315"
+    src="https://www.youtube.com/embed/Zp1FGa68mXI?loop=1&playlist=Zp1FGa68mXI"
+    frameborder="0" allowfullscreen></iframe>
