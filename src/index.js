1fc4617699d954e9a2101554d7161a3349a5afc6
Dylan Clements <dclements@protonmail.com>
2020-10-15 21:47:49 -0400

    JS TODOs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+//TODO: 
+// 1. Add field for video input
+// 2. Add function to sanitize video variable
+// 3. Add function to display video in loopPlayer div 
+
 // Test video:
 //let video = 'Zp1FGa68mXI';
 
 let video = '';
 
+
+
+// Add function here
 let videoPlayer = 
     `<iframe width="560" height="315" src="https://www.youtube.com/embed/${video}?loop=1&playlist=${video}" frameborder="0" allowfullscreen></iframe>`;
 
