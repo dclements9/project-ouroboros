02419ef5959f32b9f87f204402d5476e3ec011ce
Dylan Clements <dclements@protonmail.com>
2020-10-15 21:45:45 -0400

    videoPlayer JS structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-document.write("test");
+// Test video:
+//let video = 'Zp1FGa68mXI';
 
-const player = document.getElementById("loopPlayer")
+let video = '';
 
-document.getElementById("loopPlayer").innerHTML =  <iframe width="560" height="315"
-    src="https://www.youtube.com/embed/Zp1FGa68mXI?loop=1&playlist=Zp1FGa68mXI"
-    frameborder="0" allowfullscreen></iframe>
+let videoPlayer = 
+    `<iframe width="560" height="315" src="https://www.youtube.com/embed/${video}?loop=1&playlist=${video}" frameborder="0" allowfullscreen></iframe>`;
+
+document.getElementById("loopPlayer").innerHTML = videoPlayer;
