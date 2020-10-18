95005df5e86fb3b291c44e002c660d1f20177025
Dylan Clements <dclements@protonmail.com>
2020-10-18 09:08:21 -0400

    iframe titles

--- src/index.js
+++ src/index.js
@@ -13,24 +13,24 @@ inputButton.addEventListener('click', function() {
     }else{
         video = retrieveVideoVariable(inputLink);
         let videoPlayer = 
-            `<iframe width="560" height="315" src="https://www.youtube.com/embed/${video}?loop=1&playlist=${video}" frameborder="1" allowfullscreen></iframe>`;
+            `<iframe title="loopPlayer" width="560" height="315" src="https://www.youtube.com/embed/${video}?loop=1&playlist=${video}" frameborder="1" allowfullscreen></iframe>`;
         document.getElementById("loopPlayer").innerHTML = videoPlayer;
 
         document.cookie=`previousLink=https://www.youtube.com/embed/${video}?loop=1&playlist=${video}`;
     }
 });
 
-function retrieveVideoVariable(fullLink){
+function retrieveVideoVariable(fullLink) {
     const linkSplit = fullLink.split('=');
 
     return linkSplit[1];
 }
 
-function listPreviousLoops(){
+function listPreviousLoops() {
     const cookies = document.cookie;
     const cookiesList = cookies.split('=');
     const cookieLink = cookiesList[3];
     
-    let previousPlayer = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${cookieLink}?loop=1&playlist=${cookieLink}" frameborder="1" allowfullscreen></iframe>`;
+    let previousPlayer = `<iframe title="previousLoopPlayer" width="560" height="315" src="https://www.youtube.com/embed/${cookieLink}?loop=1&playlist=${cookieLink}" frameborder="1" allowfullscreen></iframe>`;
     document.getElementById("previousLoops").innerHTML = previousPlayer;
  }
