067892e12aee0e6aef8d4d1f26d55035a2c9244f
dclements9 <dclements@protonmail.com>
2020-11-16 10:51:12 -0500

    cleanup

--- src/index.js
+++ src/index.js
@@ -12,10 +12,7 @@ inputButton.addEventListener('click', function() {
             `<iframe title="loopPlayer" width="560" height="315" src="https://www.youtube.com/embed/${video}?loop=1&playlist=${video}" frameborder="1" allowfullscreen></iframe>`;
         document.getElementById("loopPlayer").innerHTML = videoPlayer;
 
-        document.cookie=`previousLink=https://www.youtube.com/embed/${video}?loop=1&playlist=${video}; path=/; expires=Fri, 31 Dec 9999 23:59:59 GMT"`;
-//Local Storage testing
-
-    localStorage.setItem('videoCode', `${video}`)
+        localStorage.setItem('videoCode', `${video}`)
     }
 });
 
@@ -27,11 +24,8 @@ function retrieveVideoVariable(fullLink) {
 }
 
 function listPreviousLoops() {
-
     if (localStorage.getItem('videoCode')){
         const code = localStorage.getItem('videoCode')
-        // const cookiesList = cookies.split('=');
-        // const cookieLink = cookiesList[3];
     
         let previousPlayer = `<iframe title="previousLoopPlayer" width="560" height="315" src="https://www.youtube.com/embed/${code}?loop=1&playlist=${code}" frameborder="1" allowfullscreen></iframe>`;
         document.getElementById("previousLoops").innerHTML = previousPlayer;
