4aaa03bcb27707add68e81aa87eecad06583a7df
dclements9 <dclements@protonmail.com>
2020-11-16 10:47:42 -0500

    LocalStorage test and retrieval

--- src/index.js
+++ src/index.js
@@ -13,6 +13,9 @@ inputButton.addEventListener('click', function() {
         document.getElementById("loopPlayer").innerHTML = videoPlayer;
 
         document.cookie=`previousLink=https://www.youtube.com/embed/${video}?loop=1&playlist=${video}; path=/; expires=Fri, 31 Dec 9999 23:59:59 GMT"`;
+//Local Storage testing
+
+    localStorage.setItem('videoCode', `${video}`)
     }
 });
 
@@ -25,12 +28,12 @@ function retrieveVideoVariable(fullLink) {
 
 function listPreviousLoops() {
 
-    if (document.cookie){
-        const cookies = document.cookie;
-        const cookiesList = cookies.split('=');
-        const cookieLink = cookiesList[3];
+    if (localStorage.getItem('videoCode')){
+        const code = localStorage.getItem('videoCode')
+        // const cookiesList = cookies.split('=');
+        // const cookieLink = cookiesList[3];
     
-        let previousPlayer = `<iframe title="previousLoopPlayer" width="560" height="315" src="https://www.youtube.com/embed/${cookieLink}?loop=1&playlist=${cookieLink}" frameborder="1" allowfullscreen></iframe>`;
+        let previousPlayer = `<iframe title="previousLoopPlayer" width="560" height="315" src="https://www.youtube.com/embed/${code}?loop=1&playlist=${code}" frameborder="1" allowfullscreen></iframe>`;
         document.getElementById("previousLoops").innerHTML = previousPlayer;
     }else{
         document.getElementById("previousLoops").innerHTML = "Get to Looping";
