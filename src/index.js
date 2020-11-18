18904014d987e59b5dc0b9519deefc2c87d0baf1
dclements9 <dclements@protonmail.com>
2020-11-18 09:33:18 -0500

    Fetches for one to iFrame

--- src/index.js
+++ src/index.js
@@ -33,3 +33,19 @@ function listPreviousLoops() {
         document.getElementById("previousLoops").innerHTML = "Get to Looping";
     }
  }
+
+ function testSearch(){
+    const searchTerms = ["factory%20functions","data%20structures", "array%20functions%20javascript", "composition%20over%20inheritance", "lambda%20functions", "streams%20java", "higher%20order%functions%javascript", "functional%20programming", "c++%20lambda%20functions", "sorting%20algorithms"];//a function to get a random search term
+    const getSearchTerm = () => searchTerms[Math.floor(Math.random() * (searchTerms.length-1))];
+    const YOUTUBE_API_KEY = "AIzaSyBIv9avbAnnRGx73ScR5cfTwRKu6O1WmGI";
+     const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${getSearchTerm()}&key=${YOUTUBE_API_KEY}`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {    
+        console.log(data.items[0]);
+        //console.log(data.items[0].id.videoId);
+
+        document.querySelector("#searchResultsFrame").src = `https://www.youtube.com/embed/${data.items[0].id.videoId}`;
+
+    });
+}
