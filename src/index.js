cf1983bba7bcc44a3c2b10f7b56408f5451ec414
dclements9 <dclements@protonmail.com>
2020-11-20 09:24:44 -0500

    TESTING - displayResults Working

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ inputButton.addEventListener('click', function() {
         document.getElementById("loopPlayer").innerHTML = videoPlayer;
 
         localStorage.setItem('videoCode', `${video}`)
+        
     }
 });
 
@@ -34,18 +35,62 @@ function listPreviousLoops() {
     }
  }
 
+let results = '';
+
  function testSearch(){
+     // Begin Test Terms
     const searchTerms = ["factory%20functions","data%20structures", "array%20functions%20javascript", "composition%20over%20inheritance", "lambda%20functions", "streams%20java", "higher%20order%functions%javascript", "functional%20programming", "c++%20lambda%20functions", "sorting%20algorithms"];//a function to get a random search term
     const getSearchTerm = () => searchTerms[Math.floor(Math.random() * (searchTerms.length-1))];
+    // End Test Terms
     const YOUTUBE_API_KEY = "AIzaSyBIv9avbAnnRGx73ScR5cfTwRKu6O1WmGI";
-     const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${getSearchTerm()}&key=${YOUTUBE_API_KEY}`;
+    const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${getSearchTerm()}&key=${YOUTUBE_API_KEY}`;
+
     fetch(url)
     .then(response => response.json())
     .then(data => {    
-        console.log(data.items[0]);
+        //console.log(data.items[0]);
         //console.log(data.items[0].id.videoId);
+// BEGIN WORKING
+        // results = data;
+// END WORKING
+        //document.querySelector("#searchResultsFrame").src = `https://www.youtube.com/embed/${data.items[0].id.videoId}`;
 
-        document.querySelector("#searchResultsFrame").src = `https://www.youtube.com/embed/${data.items[0].id.videoId}`;
-
+        displayResults(data)
     });
+}
+
+///////////////////BEGIN WORKING//////////////
+
+// function displayResults(){
+//     //let i = 0;
+//     console.log(results.items.length)
+//     console.log(results.items[0].snippet.thumbnails.default.url)
+
+//     for (let i = 0; i < results.items.length; i++){
+//         console.log(results.items[i]);
+//         console.log(results.items[i].snippet.thumbnails.default.url);
+        
+//         //console.log("This is source:");
+
+//         // Testing below
+//         document.getElementById('forEachResults').innerHTML += `<img src=${results.items[i].snippet.thumbnails.default.url}/>`;
+//     }
+
+// }
+
+///////////////////END WORKING//////////////
+
+
+
+
+// Below is old
+
+function displayResults(data){
+    for (let i = 0; i < data.items.length; i++){
+        console.log(data.items[i]);
+        console.log(data.items[i].snippet.thumbnails.default.url);
+
+        document.getElementById('forEachResults').innerHTML += `<img src=https://i.ytimg.com/vi/R8rmfD9Y5-c/default.jpg/>` + '<br>';
+    }
+
 }
