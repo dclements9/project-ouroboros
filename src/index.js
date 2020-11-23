ef854ecd2615c0b0bc047fbc3167c90bfb0c46de
dclements9 <dclements@protonmail.com>
2020-11-23 10:58:31 -0500

    Testing - video pass to play

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 listPreviousLoops();
 
-inputButton.addEventListener('click', function() {
+inputButton.addEventListener('click', function () {
     let video = '';
     const inputLink = document.getElementById("linkInput").value
 
-    if (inputLink === ''){
+    if (inputLink === '') {
         document.getElementById("loopPlayer").innerHTML = 'Please Enter a YouTube Link to Begin the Loop';
-    }else{
+    } else {
         video = retrieveVideoVariable(inputLink);
-        let videoPlayer = 
+        let videoPlayer =
             `<iframe title="loopPlayer" width="560" height="315" src="https://www.youtube.com/embed/${video}?loop=1&playlist=${video}" frameborder="1" allowfullscreen></iframe>`;
         document.getElementById("loopPlayer").innerHTML = videoPlayer;
 
         localStorage.setItem('videoCode', `${video}`)
-        
+
     }
 });
 
@@ -25,31 +25,31 @@ function retrieveVideoVariable(fullLink) {
 }
 
 function listPreviousLoops() {
-    if (localStorage.getItem('videoCode')){
+    if (localStorage.getItem('videoCode')) {
         const code = localStorage.getItem('videoCode')
-    
+
         let previousPlayer = `<iframe title="previousLoopPlayer" width="560" height="315" src="https://www.youtube.com/embed/${code}?loop=1&playlist=${code}" frameborder="1" allowfullscreen></iframe>`;
         document.getElementById("previousLoops").innerHTML = previousPlayer;
-    }else{
+    } else {
         document.getElementById("previousLoops").innerHTML = "Get to Looping";
     }
- }
+}
 
- function searchYT(){
+function searchYT() {
     const YOUTUBE_API_KEY = "";
 
     const searchTerm = document.getElementById("searchTermInput").value;
     const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${searchTerm}&key=${YOUTUBE_API_KEY}`;
 
     fetch(url)
-    .then(response => response.json())
-    .then(data => {    
-        displayResults(data)
-    });
+        .then(response => response.json())
+        .then(data => {
+            displayResults(data)
+        });
 }
 
-function displayResults(data){
-    for (let i = 0; i < data.items.length; i++){
+function displayResults(data) {
+    for (let i = 0; i < data.items.length; i++) {
         console.log(data.items[i]);
 
         let thumbnail = `<img src=${data.items[i].snippet.thumbnails.default.url}>`;
@@ -57,9 +57,17 @@ function displayResults(data){
         let videoID = data.items[i].id.videoId;
         let videoLink = `https://www.youtube.com/watch?v=${videoID}`
 
-        let link = `<a href=${videoLink}>${title}</a>`
-
-        document.getElementById('forEachResults').innerHTML += 
-        thumbnail + ' ' + link + '<br>' + '<br>';
+        // let link = `<a href=${videoLink} onclick="selectVideo(${videoID})">${title} </a>`
+        // let link = `<a href="#" onclick="selectVideo(${videoID});return false;">${title} </a>`
+        let link = `<a href="#" onclick="selectVideo();return false;">${title} </a>`
+        
+        document.getElementById('forEachResults').innerHTML +=
+            thumbnail + ' ' + link + '<br>' + '<br>';
     }
+}
+
+function selectVideo() {
+    console.log("made it")
+
+    return false;
 }
